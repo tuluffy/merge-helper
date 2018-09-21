2d7cfc6e8f89b54f6a0201f3b13c3631455a4284
tuluffy <tuluffy@163.com>
2018-09-21 11:27:50 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'lib/main.js',
+  output: {
+    file: 'lib/index.js',
+    format: 'cjs',
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    }),
+    commonjs(),
+  ],
+}
